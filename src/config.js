f2fd11b277f2191a57fab1816499eef2434c5d80
Pete Anderson <me@bipvanwinkle.com>
2017-11-22 00:48:41 -0700

    Allowing range from 0 to 10

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const reviewApi = ''
+export const reviewApi = 'https://us-central1-boostly-84933.cloudfunctions.net/saveReview'
 
 export const env = process.env.NODE_ENV || 'development'
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,202 +4,216 @@ import G from 'glamorous/preact';
 import { Textarea, Button, Text } from 'boostly-ui';
 import Query from 'query-string';
 import Media from 'react-media';
-import { media } from '../../settings'
-import { reviewApi, env } from '../../config'
+import { media } from '../../settings';
+import { reviewApi, env } from '../../config';
 
-const postFeedback = (orderId, reviewScore, feedbackText) => {
-    fetch(reviewApi, {
-        method: 'POST',
-        body: JSON.stringify({
-            reviewScore,
-            feedbackText
-        })
+const postFeedback = (orderId, rating, feedbackText) => {
+  const body = JSON.stringify(
+    {
+      orderId,
+      rating,
+      feedbackText,
+    },
+    null,
+    2
+  );
+  const headers = new Headers({
+    'Content-Type': 'application/json',
+  });
+  fetch(reviewApi, {
+    method: 'POST',
+    headers,
+    body,
+  })
+    .then(val => {
+      console.log(val);
     })
     .catch(err => {
-        if (env === 'development') {
-            console.log(err)
-        }
-    })
-}
+      console.log(err);
+      if (env === 'development') {
+        console.log(err);
+      }
+    });
+};
 
 const renderBoxes = onClick => {
-    const Col = G.div({
-        display: 'flex',
-        flexDirection: 'column',
-    });
-    const Row = G.div({
-        display: 'flex',
-        flexDirection: 'row',
-        margin: '40px 0px',
-        [media.phone]: {
-            margin: '20px 0px',
-        },
-    });
-    return (
-        <Media query="(max-width: 800px)">
-            {matches =>
-                matches ? (
-                    <Col>
-                        <Row>
-                            {Array(5)
-                                .fill()
-                                .map((_, i) => (
-                                    <RatingBox onClick={() => onClick(i)}>
-                                        {i + 1}
-                                    </RatingBox>
-                                ))}
-                        </Row>
-                        <Row>
-                            {Array(5)
-                                .fill()
-                                .map((_, i) => i + 5)
-                                .map(i => (
-                                    <RatingBox onClick={() => onClick(i)}>
-                                        {i + 1}
-                                    </RatingBox>
-                                ))}
-                        </Row>
-                    </Col>
-                ) : (
-                    <Row>
-                        {Array(10)
-                            .fill()
-                            .map((_, i) => (
-                                <RatingBox onClick={() => onClick(i)}>
-                                    {i + 1}
-                                </RatingBox>
-                            ))}
-                    </Row>
-                )}
-        </Media>
-    );
+  const Col = G.div({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  });
+  const Row = G.div({
+    display: 'flex',
+    flexDirection: 'row',
+    margin: '40px 0px',
+    [media.phone]: {
+      margin: '20px 0px',
+    },
+  });
+  return (
+    <Media query="(max-width: 800px)">
+      {matches =>
+        matches ? (
+          <Col>
+            <Row>
+              {Array(6)
+                .fill()
+                .map((_, i) => (
+                  <RatingBox onClick={() => onClick(i)}>
+                    {i}
+                  </RatingBox>
+                ))}
+            </Row>
+            <Row>
+              {Array(5)
+                .fill()
+                .map((_, i) => i + 6)
+                .map(i => (
+                  <RatingBox onClick={() => onClick(i)}>
+                    {i}
+                  </RatingBox>
+                ))}
+            </Row>
+          </Col>
+        ) : (
+          <Row>
+            {Array(11)
+              .fill()
+              .map((_, i) => (
+                <RatingBox onClick={() => onClick(i)}>
+                  {i}
+                </RatingBox>
+              ))}
+          </Row>
+        )}
+    </Media>
+  );
 };
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            feebackText: '',
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      feebackText: '',
+    };
+  }
 
-    handleBoxClick(queryParams) {
-        return boxIndex => {
-            this.setState({
-                reviewScore: boxIndex + 1
-            })
-            const isGoodReview = boxIndex + 1 >= 9;
-            const hasLeftReview = queryParams.hasLeftReview || false;
-            if (isGoodReview && !hasLeftReview) {
-                this.handleExternalReview(queryParams);
-            } else {
-                this.handleInternalFeedback(queryParams);
-            }
-        };
-    }
+  handleBoxClick(queryParams) {
+    return boxIndex => {
+      this.setState({
+        rating: boxIndex + 1,
+      });
+      const isGoodReview = boxIndex + 1 >= 9;
+      const hasLeftReview = queryParams.hasLeftReview || false;
+      if (isGoodReview && !hasLeftReview) {
+        this.handleExternalReview(queryParams);
+      } else {
+        this.handleInternalFeedback(queryParams);
+      }
+    };
+  }
 
-    handleInternalFeedback() {
+  handleInternalFeedback() {
+    this.setState({
+      showFeedback: true,
+    });
+  }
+
+  handleExternalReview({ reviewType, placeId }) {
+    const defaultAction = () => {
+      this.setState({
+        reviewMetadata: {
+          url: 'https://trace.com',
+        },
+      });
+    };
+    const actionSelector = {
+      google: () => {
+        const url = `https://search.google.com/local/writereview?placeid=${placeId}`;
         this.setState({
-            showFeedback: true,
+          reviewMetadata: {
+            url,
+          },
         });
-    }
+      },
+    };
+    const selectedAction = actionSelector[reviewType] || defaultAction;
+    return selectedAction();
+  }
 
-    handleExternalReview({ reviewType, placeId }) {
-        const defaultAction = () => {
-            this.setState({
-                reviewMetadata: {
-                    url: 'https://trace.com',
-                },
-            });
-        };
-        const actionSelector = {
-            google: () => {
-                const url = `https://search.google.com/local/writereview?placeid=${placeId}`;
-                this.setState({
-                    reviewMetadata: {
-                        url,
-                    },
-                });
-            },
-        };
-        const selectedAction = actionSelector[reviewType] || defaultAction;
-        return selectedAction();
+  render() {
+    const Container = G.div({
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    });
+    let queryParams = {};
+    if (typeof window !== 'undefined') {
+      queryParams = Query.parse(window.location.search);
     }
-
-    render() {
-        const Container = G.div({
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-        });
-        let queryParams = {};
-        if (typeof window !== 'undefined') {
-            queryParams = Query.parse(window.location.search);
-        }
-        const SmallContainer = G(Container)({ maxWidth: 400 });
-        if (this.state.completedFeedback) {
-            return (
-                <Text>Thank you for your feedback!</Text>
-            )
-        } else if (this.state.reviewMetadata) {
-            return (
-                <SmallContainer>
-                    <Button
-                        onClick={() => {
-                            if (typeof window !== 'undefined') {
-                                window.location.href = this.state.reviewMetadata.url;
-                            }
-                        }}>
+    const SmallContainer = G(Container)({ maxWidth: 400 });
+    if (this.state.completedFeedback) {
+      return <Text>Thank you for your feedback!</Text>;
+    } else if (this.state.reviewMetadata) {
+      return (
+        <SmallContainer>
+          <Button
+            onClick={() => {
+              if (typeof window !== 'undefined') {
+                window.location.href = this.state.reviewMetadata.url;
+              }
+            }}>
                         Review us on Google
-                    </Button>
-                </SmallContainer>
-            );
-        } else if (this.state.showFeedback) {
-            const FeedbackContainer = G(Container)({
-                height: '80%',
-                maxHeight: 300,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-around',
-                maxWidth: 600,
-            });
-            const onClick = () => {
-                //Need to get orderId from the URL, reviewScore from the state, and feedback text from the state
-                const { orderId } = typeof window !== 'undefined'
-                    ?  Query.parse(window.location.search)
-                    : {}
-                const { reviewScore, feedbackText } = this.state
-                postFeedback(orderId, reviewScore, feedbackText)
-                this.setState({
-                    feedbackText: '',
-                    completedFeedback: true
-                })
-            };
-            const onChange = (event) => {
-                this.state.feedbackText = event.target.value
-            }
-            return (
-                <FeedbackContainer>
-                    <Text>We'd love to have your feedback</Text>
-                    <Textarea
-                        value={this.state.feedbackText}
-                        height={128}
-                        onChange={onChange}
-                    />
-                    <Button onClick={onClick}>Submit</Button>
-                </FeedbackContainer>
-            );
-        }
-        const onClick = this.handleBoxClick(queryParams);
-        return (
-            <Container>
-                <Text>How would you rate us?</Text>
-                {renderBoxes(onClick)}
-            </Container>
-        );
+          </Button>
+        </SmallContainer>
+      );
+    } else if (this.state.showFeedback) {
+      const FeedbackContainer = G(Container)({
+        height: '80%',
+        maxHeight: 300,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        maxWidth: 600,
+      });
+      const onClick = () => {
+        //Need to get orderId from the URL, rating from the state, and feedback text from the state
+        const { orderId } =
+                    typeof window !== 'undefined'
+                      ? Query.parse(window.location.search)
+                      : {};
+        const { rating, feedbackText } = this.state;
+        postFeedback(orderId, rating, feedbackText);
+        this.setState({
+          feedbackText: '',
+          completedFeedback: true,
+        });
+      };
+      const onChange = event => {
+        this.state.feedbackText = event.target.value;
+      };
+      return (
+        <FeedbackContainer>
+          <Text>We'd love to have your feedback</Text>
+          <Textarea
+            value={this.state.feedbackText}
+            height={128}
+            onChange={onChange}
+          />
+          <Button onClick={onClick}>Submit</Button>
+        </FeedbackContainer>
+      );
     }
+    const onClick = this.handleBoxClick(queryParams);
+    return (
+      <Container>
+        <Text>How would you rate us?</Text>
+        {renderBoxes(onClick)}
+      </Container>
+    );
+  }
 }
 
 export default Home;
